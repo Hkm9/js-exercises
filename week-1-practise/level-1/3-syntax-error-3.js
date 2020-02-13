f87d9f94beedbb30ce068578abd36d8fb77a68cc
Hakim <hakim.mame96@gmail.com>
2020-02-13 12:32:24 +0100

    solution for week-1 & week-1 practice

--- week-1-practise/level-1/3-syntax-error-3.js
+++ week-1-practise/level-1/3-syntax-error-3.js
@@ -5,4 +5,4 @@ function(a, b, c) {
 }
 
 var result = addNumbers(1, 3, 4);
-console.log(result); // Expected result 8 - Run the program and make sure it works
+    console.log(result); // Expected result 8 - Run the program and make sure it works
